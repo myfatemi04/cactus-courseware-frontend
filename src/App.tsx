699856fe4ca42aa865b96be1a9a1081f7f602ffb
Michael Fatemi <myfatemi04@gmail.com>
2022-04-16 12:24:56 -0400

    add github loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import "./App.css";
+import { GithubFolderResponse } from "./types";
 
 function App() {
+  const [repo, setRepo] = useState("");
+  const [data, setData] = useState<null | GithubFolderResponse>(null);
+
+  const load = useCallback(() => {
+    if (!repo.includes("/")) {
+      return;
+    }
+    const [owner, name] = repo.split("/");
+    fetch(`https://api.github.com/repos/${owner}/${name}/contents/`)
+      .then((res) => res.json())
+      .then(setData);
+  }, [repo]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <input type="text" onChange={(e) => setRepo(e.target.value)} />
+        <button onClick={load}>Load course</button>
+        {data ? (
+          <>
+            {data.map((item) => {
+              return <div key={item.name}>{item.name}</div>;
+            })}
+          </>
+        ) : (
+          "No data"
+        )}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export interface Module {
+  name: string;
+  markdown: string;
+  modules: Module[];
+}
+
+export interface GithubFolderItem {
+  name: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string;
+  type: "file" | "dir";
+  _links: {
+    self: string;
+    git: string;
+    html: string;
+  };
+}
+
+export type GithubFolderResponse = GithubFolderItem[];
