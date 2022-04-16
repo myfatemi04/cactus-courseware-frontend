8e2742ef37dd7175bfef732a6822487e3e8240e5
Michael Fatemi <myfatemi04@gmail.com>
2022-04-16 15:26:28 -0400

    finish implementing questions

--- src/Question.tsx
+++ src/Question.tsx
@@ -1,13 +1,42 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
 import { Question as QuestionType } from "./types";
 
 export default function Question({ question }: { question: QuestionType }) {
   const [selectedAnswers, setSelectedAnswers] = useState<number[]>([]);
 
-  const confirm = useCallback(() => {}, []);
+  function grade() {
+    if (question.type === "single") {
+      if (selectedAnswers.length === 1) {
+        const index = selectedAnswers[0];
+        const answer = question.answers[index];
+        return answer.correct;
+      }
+      return false;
+    } else {
+      for (let i = 0; i < question.answers.length; i++) {
+        if (question.answers[i].correct !== selectedAnswers.includes(i)) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+
+  const [correct, setCorrect] = useState<boolean | null>(null);
+
+  function confirm() {
+    setCorrect(grade());
+  }
 
   return (
     <div>
+      <b>
+        {question.type === "multiple"
+          ? "Select all answers that apply."
+          : "Select the correct answer."}
+      </b>
+      <br />
       <span>{question.text}</span>
       <form style={{ display: "flex", flexDirection: "column" }}>
         {question.answers.map((answer, index) => {
@@ -18,15 +47,47 @@ export default function Question({ question }: { question: QuestionType }) {
                 type={question.type === "multiple" ? "checkbox" : "radio"}
                 name="answer"
                 id={id}
-                checked={selectedAnswers?.includes(index)}
-                onClick={(e) => {}}
+                checked={selectedAnswers.includes(index)}
+                onClick={(e) => {
+                  if (correct !== null) {
+                    return;
+                  }
+                  if (question.type === "multiple") {
+                    if (!selectedAnswers.includes(index)) {
+                      setSelectedAnswers([...selectedAnswers, index]);
+                    } else {
+                      setSelectedAnswers(
+                        selectedAnswers.filter((i) => i !== index)
+                      );
+                    }
+                  } else {
+                    setSelectedAnswers([index]);
+                  }
+                }}
               />
               <label htmlFor={id}>{answer.text}</label>
             </div>
           );
         })}
+        <div>
+          {correct === null ? (
+            ((question.type === "single" && selectedAnswers.length === 1) ||
+              (question.type === "multiple" && selectedAnswers.length > 0)) && (
+              <button onClick={confirm}>Confirm</button>
+            )
+          ) : correct === false ? (
+            <>
+              <br />❌ Incorrect.
+              <ReactMarkdown>{question.explanation}</ReactMarkdown>
+            </>
+          ) : (
+            <>
+              <br />
+              <span>🎉 Correct.</span>
+            </>
+          )}
+        </div>
       </form>
-      <button onClick={confirm}>Confirm</button>
     </div>
   );
 }
