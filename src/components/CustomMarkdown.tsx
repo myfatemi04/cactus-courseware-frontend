b2eb2c8818aed476675ee1664e4171376e96ed1e
Michael Fatemi <myfatemi04@gmail.com>
2022-04-16 23:42:34 -0400

    syntax highlighting

--- src/components/CustomMarkdown.tsx
+++ src/components/CustomMarkdown.tsx
@@ -1,14 +1,36 @@
 import ReactMarkdown from "react-markdown";
-import rehypeHighlight from "rehype-highlight";
 import rehypeMathJax from "rehype-mathjax";
 import remarkMath from "remark-math";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { twilight as theme } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-export const rehypePlugins = [rehypeMathJax, rehypeHighlight];
+export const rehypePlugins = [rehypeMathJax];
 export const remarkPlugins = [remarkMath];
 
 export default function CustomMarkdown({ children }: { children: string }) {
   return (
-    <ReactMarkdown rehypePlugins={rehypePlugins} remarkPlugins={remarkPlugins}>
+    <ReactMarkdown
+      rehypePlugins={rehypePlugins}
+      remarkPlugins={remarkPlugins}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              children={String(children).replace(/\n$/, "")}
+              style={theme}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {children}
     </ReactMarkdown>
   );
