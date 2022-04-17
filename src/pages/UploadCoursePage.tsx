f3eba66638d7b2ac2c6e845b136074fc21bb66f4
Claeb101 <calebarulandu@gmail.com>
2022-04-17 05:12:58 -0400

    course upload styling

--- src/pages/UploadCoursePage.tsx
+++ src/pages/UploadCoursePage.tsx
@@ -4,19 +4,23 @@ import { publishCourse } from "../services/api";
 import { FetchStatus } from "../types";
 import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import bgImg from '../assets/bg.png';
 
-const UploadFeedback = ({status}: { status: FetchStatus }) => {
+const Toggle = ({status}: { status: FetchStatus }) => {
   let item = null; 
   switch(status) {
+    case 'idle':
+      item = <ChevronRightIcon style={{margin: 'auto 0', fontSize: '2rem'}} color='warning'/>
+      break;
     case 'pending':
       item = <CircularProgress color='warning' style={{margin: 'auto 1rem auto 0'}} size='2rem'/>
       break;
     case 'success':
-      item =  <CheckIcon style={{margin: 'auto 1rem auto 0', fontSize: '2rem'}} color='warning'/>
+      item =  <CheckIcon style={{margin: 'auto 0', fontSize: '2rem'}} color='warning'/>
       break;
     case 'error':
-      item =  <ClearIcon style={{margin: 'auto 1rem auto 0', fontSize: '2rem'}} color='warning'/>
+      item =  <ClearIcon style={{margin: 'auto 0', fontSize: '2rem'}} color='warning'/>
       break;
   }
 
@@ -56,35 +60,23 @@ export default function UploadCoursePage() {
   );
 
   return (
-    <div className=" h-screen" style={{backgroundImage: `url(${bgImg})`}}>
-      <h1 className=" text-white text-7xl font-bold">Upload a GitHub Repo</h1>
-      <p className="text-white text-3xl font-light">See our example repository for a easy-to-use, detailed <b className="font-bold">specification</b></p>
-      
-      <div style={{ margin: "auto 3rem" }}>
-      <div style={{ display: "flex" }}>
-        <TextField
-          id="outlined-basic"
-          variant="outlined"
-          color="primary"
-          label="Search"
-          placeholder="user-name/repo-name"
-          value={urlInput}
-          onKeyUp={onKeyUp}
-          onChange={(e)=>setUrlInput(e.target.value)}
-          error={errorMsg !== ''}
-          helperText={errorMsg !== '' ? errorMsg : null}
-          style={{width: '36rem', height: '100%', padding: 0}}
-        />
+    <div className="relative h-screen" style={{backgroundImage: `url(${bgImg})`}}>
+      <div className="absolute left-8 bottom-8">
+        <h1 className="mb-4 text-white text-7xl font-bold">Upload a GitHub Repo</h1>
+        <p className="mb-8 text-white text-3xl font-light">See our example repository for a easy-to-use, detailed <b className="font-bold">specification</b></p>
+        
+        <div className="relative w-full h-full">
+          <input
+            type="text"
+            onChange={(e) => setUrlInput(e.target.value)}
+            onKeyUp={onKeyUp}
+            className="mb-8 text-white text-6xl font-light border-white border-b-2 bg-transparent"
+          />
+          <button className="w-12 h-12 " onClick={() => publish()}><Toggle status={fetchStatus}/></button>
+          {errorMsg.length > 0 ? <p className="absolute bottom-0 text-coral">{errorMsg}</p> : null}
+        </div>
 
-        <Button
-          variant="contained"
-          onClick={() => publish()}
-          style={{marginLeft: '1rem', 'height': '3.5rem'}}
-        ><UploadFeedback status={fetchStatus}/> Publish</Button>
       </div>
-      
     </div>
-    </div>
-    
   );
 }
