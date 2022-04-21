180b8b7d9ba1b654dfc06ca72687a50a8168bf2d
Michael Fatemi <myfatemi04@gmail.com>
2022-04-20 22:12:41 -0400

    fix path bug

--- src/lib/pathutil.ts
+++ src/lib/pathutil.ts
@@ -28,17 +28,29 @@ export function getNextPath(
   return null;
 }
 
+export function getPathToLastItem(module: Module) {
+  const path = [];
+  let currentModule = module;
+  while (currentModule.children.length > 0) {
+    path.push(currentModule.children.length - 1);
+    currentModule = currentModule.children[currentModule.children.length - 1];
+  }
+  return path;
+}
+
 export function getPreviousPath(
   rootModule: Module,
   path: number[]
 ): number[] | null {
-  // Returns either null or a path to the next module
-  // null if there is no path to the next module
+  console.log(rootModule, path);
+
+  // Returns either null or a path to the previous module
+  // null if there is no path to the previous module
   if (path.length === 0) {
     return null;
   }
 
-  if (path[0] === 0) {
+  if (path.length === 1 && path[0] === 0) {
     return [];
   }
 
@@ -50,7 +62,11 @@ export function getPreviousPath(
     return [path[0], ...previousPathForSubmodule];
   } else {
     if (path[0] - 1 >= 0) {
-      return [path[0] - 1];
+      // Find the last module within that module
+      return [
+        path[0] - 1,
+        ...getPathToLastItem(rootModule.children[path[0] - 1]),
+      ];
     }
   }
 
